e87fd99a0d1fd19d4c611f834ef28f1b5dd58cc8
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-02-12 18:49:47 +0530

    pgi

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import HomeClient from "@/components/HomeClient";
 
 // SEO Metadata for Home Page
 export const metadata = {
-  title: "DoctorDesk - Book Doctor Appointment, Online Doctor Consultation",
+  title: "DoctorDesk - Video Consultation with Doctors, Book Doctor",
   description:
     "DoctorDesk helps doctors and independent professionals manage appointments, patient records, bills, and practice workflows effortlessly. Join India's top healthcare SaaS platform today.",
   alternates: {
